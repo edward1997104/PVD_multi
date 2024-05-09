8b92cd092ecba5e77798bff6293d95a5dd2c4750
Ka Hei Hui <khui@nvidia.com>
2024-05-09 16:31:35 +0800

    1. run all

---
+++ run_all_inference.sh
@@ -0,0 +1,3 @@
+bash run_inference.sh chair ./ckpt/generation/chair_1799.pth chair_outputs
+bash run_inference.sh airplane ./ckpt/generation/airplane_2899.pth airplane_outputs
+bash run_inference.sh car ./ckpt/generation/car_3999.pth car_outputs
